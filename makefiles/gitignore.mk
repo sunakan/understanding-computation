0bf79be68db3a22c4f6c20a4088cfed3969e9411
sunakan <sunakan03@gmail.com>
2020-08-06 08:58:19 +0000

    準備

---
+++ makefiles/gitignore.mk
@@ -0,0 +1,17 @@
+################################################################################
+# 変数
+################################################################################
+IGNORE_OS       := linux,macos,windows
+IGNORE_EDITOR   := vim,emacs,intellij+all,visualstudiocode
+IGNORE_LANGUAGE := bash,ruby
+IGNORE_LIST     := $(IGNORE_LANGUAGE),$(IGNORE_OS),$(IGNORE_EDITOR)
+GIT_IGNORE_URL  := https://www.toptal.com/developers/gitignore/api/$(IGNORE_LIST)
+
+################################################################################
+# タスク
+################################################################################
+.gitignore:
+	curl --output .gitignore $(GIT_IGNORE_URL)
+
+.PHONY: setup-gitignore
+setup-gitignore: .gitignore ## .gitignoreをsetup
